18cc38938a0b3e83d2944cf0a0f9b1147e44f88d
Mujithaba <mujithabaep772@gmail.com>
2024-10-08 09:16:38 +0530

    axios error fix

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,20 +1,4 @@
-// import axios from 'axios';
 
-// const API_BASE_URL = 'http://localhost:3000/api/pdf';
-
-// const pdfApi = axios.create({
-//   baseURL: API_BASE_URL,
-// });
-
-// pdfApi.interceptors.response.use(
-//   response => response,
-//   error => {
-//     console.error('API Error:', error);
-//     return Promise.reject(error);
-//   }
-// );  
-
-// export default pdfApi;
 
 import axios from 'axios';
 
